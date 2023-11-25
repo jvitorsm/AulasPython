22a638adc9fb6bc8203447579960634e44917b38
jvitorsm <joaovsm04@gmail.com>
2023-11-24 22:38:45 -0300

    Commit 24112023

---
+++ Map.py
@@ -0,0 +1,15 @@
+# map() =   applies a function to each item in an iterable (lsit, tuple, etc...)
+# map(function,itereble)
+
+store = [('shirt',20.00),
+         ('pants',25.00),
+         ('jacket',50.00),
+         ('socks',10.00)]
+
+to_euros = lambda data: (data[0],data[1]*0.82)
+# to_dolar =  lambda data: (data[0],data[1]/0.82)
+
+store_euros = list(map(to_euros, store))
+
+for i in store_euros:
+    print(i)
