b4912a3500a9931a2c4cdb57a10962b632df6121
jvitorsm <joaovsm04@gmail.com>
2023-11-27 22:32:25 -0300

    Commit 27112023.2

---
+++ SortInList.py
@@ -0,0 +1,15 @@
+# sort() method     = used with lists
+
+students = ['Squidward', 'Sandy', 'Patrick','Spongebob', 'Mr.Krabs']
+
+students.sort()
+
+for i in students:
+    print(i)
+print('*' * 50)
+students = ['Squidward', 'Sandy', 'Patrick','Spongebob', 'Mr.Krabs']
+
+students.sort(reverse=True)
+
+for i in students:
+    print(i)
